3969ced0d3c4e8c726456050ef9720048b573345
Jonathan Filippini <filippini.jo@gmail.com>
2011-08-30 19:35:06 +0200

    lib included

--- parser/Main.java
+++ parser/Main.java
@@ -1,12 +1,23 @@
 /* Esercitazione 2 - Esercizio 1 (Main) */
+
+import it.polito.lt.skype.command.Utility;
 import java.io.*;
 import java_cup.parser;
+import java_cup.runtime.*;
+
+
    
 public class Main {
   static public void main(String argv[]) {    
     try {
       /* Istanzio lo scanner aprendo il file di ingresso argv[0] */
-      Lexer l = new Lexer(new FileReader(argv[0]));
+        
+      JFlex.Main.generate(new File("parser/scanner.jflex"));       
+      
+      Utility.mf("inserisci comando:\n ");
+      
+      
+      Lexer l = new Lexer(System.in);
       /* Istanzio il parser */
       parser p = new parser(l);
       /* Avvio il parser */
