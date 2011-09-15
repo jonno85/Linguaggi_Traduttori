8927e03f7897c7f015083f608aca3f9d3bcd33b8
robizz <grobizz@gmail.com>
2011-09-15 16:06:42 +0200

    eliminata robbaccia

--- drawTree/cmd (copia).sh
+++
@@ -1,9 +0,0 @@
-#! /bin/tcsh -f
-#source ~/POLITO/Linguaggi_Traduttori/drawTree/ambiente_traduttori
-rm *.class;
-rm Lexer.java parser.java sym.java Lexer.java~
-jflex scanner.jflex;
-java java_cup/MainDrawTree parser_clean.cup;
-
-javac *.java;
-java Main ../input/prova.txt;
--- drawTree/cmd.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/tcsh -f
-#source ~/POLITO/Linguaggi_Traduttori/drawTree/ambiente_traduttori
-jflex scanner_clean.jflex;
-java java_cup/MainDrawTree parser_clean.cup;
-javac *.java;
-java Main ../input/prova.txt;
